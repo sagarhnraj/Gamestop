2165d5e0dcb510342941609255563736699bd99e
sagarhnraj <sagarhnagaraj@gmail.com>
2026-08-12 09:31:17 +0530

    debug: enable RenderNetworkDiagnostic to diagnose startup environment

--- Gamestop/backend/src/main/java/com/sg/gamestopbackend/GamestopbackendApplication.java
+++ Gamestop/backend/src/main/java/com/sg/gamestopbackend/GamestopbackendApplication.java
@@ -11,6 +11,7 @@ public class GamestopbackendApplication {
 
     public static void main(String[] args) {
         try {
+            RenderNetworkDiagnostic.runDiagnostic();
             SpringApplication.run(GamestopbackendApplication.class, args);
         } catch (Throwable t) {
             System.err.println("=== FATAL STARTUP EXCEPTION ===");
